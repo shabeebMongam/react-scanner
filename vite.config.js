fa7518cf6370d7786ed322a349250a0827041520
shabeeb <shabeebmongam@gmail.com>
2024-10-17 16:27:09 +0530

    add homepage

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/react-qr-scanner",
   plugins: [react()],
+  base:"/react-qr-scanner/",
 })
