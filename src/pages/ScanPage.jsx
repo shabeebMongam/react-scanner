ede0ce49e5e5224f2e00979b3e75f84a844c45c0
shabeeb <shabeebmongam@gmail.com>
2024-10-17 17:50:57 +0530

    same as last commit

--- src/pages/ScanPage.jsx
+++ src/pages/ScanPage.jsx
@@ -3,8 +3,10 @@ import MyQrScanner from '../components/general/qrScanner/MyQrScanner'
 
 const ScanPage = () => {
   return (
-    <div className='border-black border-4 h-full w-full flex justify-center items-center'>
+    <div className='border-black border-4 h-full w-full flex justify-center pt-20'>
+      <div className='rounded-3xl'> 
         <MyQrScanner/>
+      </div>
     </div>
   )
 }
